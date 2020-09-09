af266de612d22b7a177ff11f91e69a82ee418145
Jamie Lopez <jmelopez@outlook.com>
2020-09-09 18:15:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(){
   return kittens.pop()
 }
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
